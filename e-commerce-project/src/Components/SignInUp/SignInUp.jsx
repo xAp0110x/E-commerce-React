9179464be2f0e960ae86ec5bf361a5626929810b
xAp0110x <elmar.bashirov13@gmail.com>
2024-07-28 19:47:39 +0400

    SignUp and animations added

--- e-commerce-project/src/Components/SignInUp/SignInUp.jsx
+++ e-commerce-project/src/Components/SignInUp/SignInUp.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../Styles/SignInUp.css'
 import { FaUser } from "react-icons/fa";
 import { RiLockPasswordFill } from "react-icons/ri";
+import { MdOutlineAlternateEmail } from "react-icons/md";
 
 const SignInUp = () => {
+
+    const [action, setAction] = useState('');
+
+    const signupLink = () => {
+        setAction(' active');
+    }
+
+    const signinLink = () => {
+        setAction('');
+    }
+
     return (
         <div className="sign-in-up-body">
-            <div className='sign-in-up-wrapper'>
+            <div className={`sign-in-up-wrapper${action}`}>
+                {/* sign in form */}
                 <div className="form-block signin">
                     <form action="">
                         <h1>Sign In</h1>
@@ -31,7 +44,35 @@ const SignInUp = () => {
                         <button type='submit'>Sign In</button>
 
                         <div className='register-link'>
-                            <p>Create a new account: <a href="#">Sign Up</a> </p>
+                            <p>Create a new account: <a href="#" onClick={signupLink} >Sign Up</a> </p>
+                        </div>
+                    </form>
+                </div>
+
+                {/* sign up form */}
+                <div className="form-block signup">
+                    <form action="">
+                        <h1>Sign Up</h1>
+
+                        <div className="input-box">
+                            <input type="text" placeholder='Username' required />
+                            <FaUser className='icon' />
+                        </div>
+
+                        <div className="input-box">
+                            <input type="email" placeholder='E-mail adress' required />
+                            <MdOutlineAlternateEmail className='icon' />
+                        </div>
+
+                        <div className="input-box">
+                            <input type="password" placeholder='Password' required />
+                            <RiLockPasswordFill className='icon' />
+                        </div>
+
+                        <button type='submit'>Sign Up</button>
+
+                        <div className='register-link'>
+                            <p>Already have an account: <a href="#" onClick={signinLink} >Sign In</a> </p>
                         </div>
                     </form>
                 </div>
