5df10b092d0e0033a182fabbe4176e8f9427871b
xAp0110x <elmar.bashirov13@gmail.com>
2024-07-31 18:37:03 +0400

    Products added & developing ProductCards

--- e-commerce-project/src/App.js
+++ e-commerce-project/src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
+import React from 'react';
 import SignInUp from './Components/SignInUp/SignInUp';
+import StandardCard from './Components/ProductCards/StandardCard';
+import products from './data/products';
 
 function App() {
+
+  // const handleCardClick = (product) => {
+  //   alert(`Product clicked: ${product.name}`);
+  // }; testing product cards
+
   return (
     <div className="App">
-      <SignInUp />
+      {/* <SignInUp /> component test */}
+
+      {/* {products.map((product) => (
+        <StandardCard
+          key={product.id}
+          product={product}
+          onClick={handleCardClick}
+        />
+      ))} component test */}
+
+
     </div>
   );
 }
---
+++ e-commerce-project/src/Components/ProductCards/StandardCard.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import '../../Styles/StandardCard.css';
+
+const StandardCard = ({ product, onClick }) => {
+    return (
+        <div className="standard-card" onClick={() => onClick(product)}>
+            <img 
+                src={product.image}
+                alt={product.name} 
+                className="product-image" 
+            />
+            
+            <div className="product-info">
+                <h3 className="product-name">{product.name}</h3>
+                <p className="product-price">${product.price}</p>
+            </div>
+        </div>
+    );
+};
+
+StandardCard.propTypes = {
+    product: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        image: PropTypes.string.isRequired,
+        price: PropTypes.number.isRequired,
+    }).isRequired,
+    onClick: PropTypes.func.isRequired,
+};
+
+export default StandardCard;
